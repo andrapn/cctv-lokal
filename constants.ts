5b3caef6ac2dfc7f869931d0e7162769f985a20a
andrapn <andraput988@gmail.com>
2026-02-15 16:59:48 +0700

    maintenance

--- constants.ts
+++ constants.ts
@@ -25,42 +25,42 @@ export const CAMERAS: CameraConfig[] = [
     id: 'cam-03',
     label: 'Jalan Jerakah',
     location: 'Persimpangan',
-    url: 'http://localhost:8888/cctv_jrakah/index.m3u8',
+    url: 'https://cctv.manalagi.cloud/cctv_jrakah/index.m3u8',
     category: 'kota'
   },
   {
     id: 'cam-04',
     label: 'Jalan Krapyak',
     location: 'Persimpangan',
-    url: 'http://localhost:8888/cctv_krapyak/index.m3u8',
+    url: 'https://cctv.manalagi.cloud/cctv_krapyak/index.m3u8',
     category: 'kota'
   },
   {
     id: 'cam-05',
     label: 'Jalan Kalibanteng',
     location: 'Columbia Asia',
-    url: 'http://localhost:8888/cctv_kalibanteng/index.m3u8',
+    url: 'https://cctv.manalagi.cloud/cctv_kalibanteng/index.m3u8',
     category: 'kota'
   },
   {
     id: 'cam-06',
     label: 'Tugu Muda',
     location: 'Pemuda',
-    url: 'http://localhost:8888/cctv_tugumuda/index.m3u8',
+    url: 'https://cctv.manalagi.cloud/cctv_tugumuda/index.m3u8',
     category: 'kota'
   },
   {
     id: 'cam-07',
     label: 'Simpang Lima',
     location: 'Jalan Utama Depan Masjid',
-    url: 'http://localhost:8888/cctv_simpanglima/index.m3u8',
+    url: 'https://cctv.manalagi.cloud/cctv_simpanglima/index.m3u8',
     category: 'kota'
   },
   {
     id: 'cam-08',
     label: 'Jalan Pahlawan',
     location: '180 derajat Pahlawan',
-    url: 'http://localhost:8888/cctv_pahlawan/index.m3u8',
+    url: 'https://cctv.manalagi.cloud/cctv_pahlawan/index.m3u8',
     category: 'kota'
   }
 ];
